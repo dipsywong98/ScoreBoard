29de288855c94ddc3495c4f21426d2e7fecea97d
Daniel Cheung <daniel.2345@hotmail.com>
2018-11-22 23:37:37 +0800

    Fixed team name location

--- src/components/TeamColumn.vue
+++ src/components/TeamColumn.vue
@@ -54,7 +54,7 @@
 
         .name {
             position: absolute;
-            top: 6vh;
+            top: 6.5vh;
             font-size: 6vh;
             text-shadow: 0 0.5vh 0.5vh rgba(0, 0, 0, 0.4);
             width: 30vw;
